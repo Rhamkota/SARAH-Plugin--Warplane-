78c909a3d12364b7de8165d2c6faece0dc0bb41c
Rhamkota <adri1gamers@hotmail.Com>
2014-04-18 23:32:44 +0200

    Create warplane.js

---
+++ warplane.js
@@ -0,0 +1,24 @@
+exports.action = function(data, callback, config, SARAH) {
+  
+var exec = require('child_process').exec;
+ 
+switch(data.val) {
+	case "runwarplane":
+	  var process = '%CD%/plugins/warplane/ahk/Runwarplane.ahk';
+	  var tts = "c'est fait monsieur bon vol";
+	  break;
+	case "closewarplane":
+	  var process = '%CD%/plugins/warplane/ahk/Closewarplane.ahk';
+	  var tts = "Combat terminé monsieur";
+	  break;  
+  }    
+
+ var child = exec(process,
+  function (error, stdout, stderr) {
+    console.log(process);
+   });
+  
+  callback({'tts': tts});
+
+ }
+ 
